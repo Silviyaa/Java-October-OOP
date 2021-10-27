529a7637696ea60c8bb95255d70e13f48f421f87
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2021-10-27 10:08:51 +0300

    Working with Abstraction
    Working with Abstraction
    
    • Project Architecture;
    • Code Refactoring;
    • Enumerations;
    • Static Keyword;
    • Java Packages,

---
+++ 1. Working with Abstraction/Lab/src/enumerations/DayOfWeek.java
@@ -0,0 +1,21 @@
+package enumerations;
+
+public enum DayOfWeek {
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday");
+
+    private String name;
+
+    private DayOfWeek(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
